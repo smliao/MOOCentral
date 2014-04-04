3ff4e147272c23bccfc7012dc60ca6a3a3c79e1d
smliao <stevenliao@Stevens-MacBook-Pro-4.local>
2014-04-04 16:05:10 -0700

    updated what data to display on website for Open2Study

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <html>
+	<head>
+	</head>
 
 <body>
+	<div align="center">
+	<form>
+		<input name="keyword" type="text" placeholder="Search for a course" />
+		<input type="submit" value="Query" />
+	</form>
+	</div>
 
 <?php
 	$con = mysqli_connect("localhost", "root", "", "MOOCentral");
@@ -15,9 +23,14 @@
 		<th> ID </th>
 		<th> TITLE </th>
 		<th> short_desc </th>
-		<th> long_desc </th>
 		<th> course_link </th>
-		<th> video_link </th>
+		<th> start_date </th>
+		<th> course_length </th>
+		<th> course_image </th>
+		<th> category </th>
+		<th> site </th>
+		<th> profname </th>
+		<th> profimage </th>
 		</tr>";
 
 	while($row = mysqli_fetch_array($data)){
@@ -25,17 +38,30 @@
 		echo "<td>" . $row['id'] . "</td>";
 		echo "<td>" . $row['title'] . "</td>";
 		echo "<td>" . $row['short_desc'] . "</td>";
-		echo "<td>" . $row['long_desc'] . "</td>";
+		// echo "<td>" . $row['long_desc'] . "</td>";
 		echo "<td><a href=\"" . $row['course_link'] .  "\" target=\"_blank\">Course Link</a></td>";
+		
+		/* removed video for faster loading
 		echo "<td> 
 			<iframe title=\"Youtube player\" width=\"480\" height=\"390\"
 			src=\"http:" . $row['video_link'] . "\" 
 			frameborder =\"0\" allowfullscreen></iframe></td>";
+		*/
+
+		echo "<td>" . $row['start_date'] . "</td>";
+		echo "<td>" . $row['course_length'] . "</td>";
+		echo "<td>" . $row['course_image'] . "</td>";
+		echo "<td>" . $row['category'] . "</td>";
+		echo "<td>" . $row['site'] . "</td>";
+		echo "<td>" . $row['profname'] . "</td>";
+		echo "<td><image src=\"" . $row['profimage'] . "\" alt=\"profimage\"></td>";
 
 		echo "</tr>";
 	}
 	echo "</table>";
 ?>
+
+
 
 </body>>
 
