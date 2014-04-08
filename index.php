5ddfcdc78d2be790c33f571b1e1f4d623d8402bc
smliao <stevenliao@Stevens-MacBook-Pro-4.local>
2014-04-07 21:39:17 -0700

    Updated code for server

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 	</div>
 
 <?php
-	$con = mysqli_connect("localhost", "root", "", "MOOCentral");
+	$con = mysqli_connect("localhost", "sjsucsor_s2g414s", "abcd#1234", "sjsucsor_160s2g42014s");
+	//$con = mysqli_connect("localhost", "root", "", "MOOCentral");	
 	if(mysqli_connect_errno()){
 		echo "failed to connect to MySQL: " . mysqli_connect_errno();
 	}
