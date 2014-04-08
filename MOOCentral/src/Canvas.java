f52c22807782ded3b7f8787fd62fb5eb42593d13
Sander Peerna <rednas21@gmail.com>
2014-04-07 18:21:38 -0700

    Start date, course length, fees

--- MOOCentral/src/Canvas.java
+++ MOOCentral/src/Canvas.java
@@ -3,6 +3,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -15,18 +17,14 @@ public class Canvas {
 	 * @throws IllegalAccessException 
 	 * @throws InstantiationException 
 	 * @throws SQLException 
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, ParseException {
 		String url = "https://www.canvas.net/";		
 		 
-		ArrayList<String> pgcrs = new ArrayList<String>(); //Array which will store each course URLs 
+		ArrayList<String> pgcrs = new ArrayList<String>();
 		pgcrs.add(url);
-		 //The following few lines of code are used to connect to a database so the scraped course content can be stored.
-		//Class.forName("com.mysql.jdbc.Driver").newInstance();
-		//java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/scrapedcourse","root","");
-		//make sure you create a database named scrapedcourse in your local mysql database before running this code
-		//default mysql database in your local machine is ID:root with no password
-		//you can download scrapecourse database template from your Canvas account->modules->Team Project area
+		
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/MOOCentral","root","root");
 		Statement statement = connection.createStatement();
@@ -42,15 +40,14 @@ public class Canvas {
 			
 			for (int j=0; j < link.size(); j++)
 			{
-				//Statement statement = connection.createStatement();
 				//Course Name
 				String CrsName = link.get(j).attr("title");
-	//print			System.out.println("Title:       " + CrsName);
+//				System.out.println("Title:       " + CrsName);
 				CrsName = CrsName.replace("'", "''");
 				CrsName = CrsName.replace(",", "");
 				//Course URL
 				String CrsURL = link.get(j).attr("href");
-	//print			System.out.println("URL:         " + CrsURL);
+//				System.out.println("URL:         " + CrsURL);
 				//Connect to the course page
 				Document crsdoc = Jsoup.connect(CrsURL).get();
 				//Get the course description element
@@ -59,16 +56,16 @@ public class Canvas {
 				CrsDesc = CrsDesc.replace("?", "");
 				CrsDesc = CrsDesc.replace("'", "''");
 				CrsDesc = CrsDesc.replace(",", "");
-	//print			System.out.println("Description: " + CrsDesc);
+//				System.out.println("Description: " + CrsDesc);
 				//Get the course image
 				Element img = crsdoc.select("meta[property=og:image:secure_url]").first();
 				String CrsImg = img.attr("content");
-	//print			System.out.println("Image:       " + CrsImg);
+//				System.out.println("Image:       " + CrsImg);
 				//Get the course school image
 				Element schoolimg = crsdoc.select("div.school-logo > img").first();
 				String CrsSchoolImg = schoolimg.attr("src");
 				CrsSchoolImg = "https://www.canvas.net" + CrsSchoolImg;
-	//print				System.out.println("School Logo: " + CrsSchoolImg);
+//				System.out.println("School Logo: " + CrsSchoolImg);
 				//Get the professor logo
 				//Get the course school image
 				Element profimg = crsdoc.select("div.instructor-bio > img").first();
@@ -84,86 +81,61 @@ public class Canvas {
 				}
 				String CrsProfName = profname.text();
 				CrsProfImg = "https://www.canvas.net" + CrsProfImg;
-	//print			System.out.println("Professor:   " + CrsProfName + " - " + CrsProfImg);
-	//print			System.out.println("-----------------------------------------------");
-				Elements crsheadele = crsdoc.select("div[class=gray-noise-box pad-box no-sides]");
-				String youtube = "write your own code";
-				Elements crsbodyele = crsdoc.select("div[class=light-bg pad-box no-sides top-rule-box]");
-				String CrsDes = "write your own code";
-				CrsDes = CrsDes.replace("'", "''");
-				CrsDes = CrsDes.replace(",", "");
-				if(CrsDes.contains("?"))
+//				System.out.println("Professor:   " + CrsProfName + " - " + CrsProfImg);
+				//Get the course date, duration, and cost.
+				Element date = crsdoc.select("div.course-detail-info > p").first();
+				String CrsInfo = date.text();
+				String CrsDate = CrsInfo.substring(0, CrsInfo.indexOf(" Cost"));
+				String CrsEnd = "N/A";
+				String CrsLength = "N/A";
+				SimpleDateFormat convertToDate = new SimpleDateFormat("MMM d, yyyy");
+				SimpleDateFormat convertToSQLDate = new SimpleDateFormat("yyyy-MM-dd");
+				if(CrsDate.contains("Self-paced"))
 				{
-					CrsDes = CrsDes.replace("?", "");
+					CrsDate = CrsDate.substring(22);
+					java.util.Date startDate = convertToDate.parse(CrsDate);
+					CrsDate = convertToSQLDate.format(startDate);
+					CrsLength = "Self-paced";
 				}
+				else if(CrsDate.contains("Coming"))
+				{
+					CrsDate = CrsDate.substring(7);
+					java.util.Date startDate = convertToDate.parse(CrsDate);
+					CrsDate = convertToSQLDate.format(startDate);
+				}
+				else
+				{
+					CrsEnd = CrsDate.substring(CrsDate.indexOf("to") + 3);
+					CrsDate = CrsDate.substring(0, CrsDate.indexOf(" to"));
+					java.util.Date startDate = convertToDate.parse(CrsDate);
+					java.util.Date endDate = convertToDate.parse(CrsEnd);
+					CrsDate = convertToSQLDate.format(startDate);
+					CrsEnd = convertToSQLDate.format(endDate);
+					long startTime = startDate.getTime();
+					long endTime = endDate.getTime();
+					long diffTime = endTime - startTime;
+					diffTime = (diffTime / (1000 * 60 * 60 * 24)) / 7;
+					CrsLength = diffTime + " weeks";
+				}
+				String CrsFee = CrsInfo.substring(CrsInfo.indexOf("ment:") + 6);
+//				System.out.println("Start Date:  " + CrsDate);
+//				System.out.println("End Date:    " + CrsEnd);
+//				System.out.println("Duration:    " + CrsLength);
+//				System.out.println("Fee:         " + CrsFee);
+//				System.out.println("-----------------------------------------------");
 				
-				String video_link = "does not exist";
-				String start_date = "1900-01-01";
-				String course_length = "have to compute";
-				String category = "does not exist";
-				String site = url;
+				String video_link = "N/A";
+				String category = "N/A";
+				String site = "Canvas";
 				
 				String query = "INSERT INTO `MOOCentral`.`coursedata` (`id`, `title`, `short_desc`,`course_link`, `video_link`, `start_date`, `course_length`, `course_image`, `category`, `site`, `profname`, `profimage` )" + 
-						"VALUES(NULL, '" + CrsName + "','" + CrsDesc + "','" + CrsURL + "','" + video_link + "','" + start_date + "','" + course_length + "','" + CrsImg + "','" + category + "','" + site + "','" + CrsProfName + "','" + CrsProfImg + "')";
+						"VALUES(NULL, '" + CrsName + "','" + CrsDesc + "','" + CrsURL + "','" + video_link + "','" + CrsDate + "','" + CrsLength + "','" + CrsImg + "','" + category + "','" + site + "','" + CrsProfName + "','" + CrsProfImg + "')";
 				
 				System.out.println(query);
 				statement.executeUpdate(query);
-				
-				
-				
-//				String Date = crsdoc.select("h4[class=emboss-light] > p").text();
-//				String StrDate = Date.substring(Date.indexOf(":")+1, Date.length()); //Start date after the :
-//				String datechk = StrDate.substring(0, StrDate.indexOf(" "));
-//				if(!datechk.matches(".*\\d.*"))
-//				{
-//					if(StrDate.contains("n/a"))
-//					{
-//						StrDate = "write you own code";
-//					}
-//					else
-//					{
-//						StrDate = "write your own code";
-//					}
-//				}
-//				else
-//				{
-//					String date = StrDate.substring(0, StrDate.indexOf(" "));
-//					String month = StrDate.substring(StrDate.indexOf(" ")+1, StrDate.indexOf(" ")+4);
-//					String year = StrDate.substring(StrDate.length()-4,StrDate.length());
-//					StrDate = "write your own code";
-//				}
-//				Element chk = crsdoc.select("div[class=effort last]").first();
-//				Element crslenschk = crsdoc.select("div[class*=duration]").first();
-//				String crsduration;
-//				if (crslenschk==null)
-//				{
-//					crsduration = "0";
-//				}
-//				else if(StrDate.contains("n/a self-paced"))
-//				{
-//					crsduration = "0";
-//				}
-//				else
-//				{
-//					try{
-//						String crsdurationtmp = crsdoc.select("div[class*=duration]").text();
-//						int start = crsdurationtmp.indexOf(":")+1;
-//						int end = crsdurationtmp.indexOf((" "),crsdurationtmp.indexOf(":"));
-//						crsduration = crsdurationtmp.substring(start, end);
-//					}
-//					catch (Exception e)
-//					{
-//						crsduration ="0";
-//						System.out.println("Exception");
-//					}
-//				}
-				//The following is used to insert scraped data into your database table. Need to uncomment all database related code to run this.
-//				String query = "insert into course_data values(null,'"+CourseName+"','"+SCrsDesrpTemp+"','"+CrsDes+"','"+crsurl+"','"+youtube+"',"+StrDate+","+crsduration+",'"+CrsImg+"','','Edx')";
-//				System.out.println(query);                	
-				//statement.executeUpdate(query);// skip writing to database; focus on data printout to a text file instead.
-				//statement.close(); 
+//				statement.close(); 
 			 }
 		}
-		//connection.close();   
+		connection.close();   
 	}
 }
