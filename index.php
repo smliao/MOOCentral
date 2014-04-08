a6f1d92e7404b4cf9d1cfa286524a1cafe31b749
smliao <stevenliao@Stevens-MacBook-Pro-4.local>
2014-04-07 22:38:09 -0700

    fixed minor stuff

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	</head>
 
 <body>
+
+	<h1> MOOCentral </h1>
 	<div align="center">
 	<form>
 		<input name="keyword" type="text" placeholder="Search for a course" />
@@ -21,14 +23,10 @@
 
 	echo "<table border = '1' 
 		<tr>
-		<th> ID </th>
 		<th> TITLE </th>
-		<th> short_desc </th>
-		<th> course_link </th>
 		<th> start_date </th>
 		<th> course_length </th>
 		<th> course_image </th>
-		<th> category </th>
 		<th> site </th>
 		<th> profname </th>
 		<th> profimage </th>
@@ -36,11 +34,11 @@
 
 	while($row = mysqli_fetch_array($data)){
 		echo "<tr>";
-		echo "<td>" . $row['id'] . "</td>";
+	//	echo "<td>" . $row['id'] . "</td>";
 		echo "<td>" . $row['title'] . "</td>";
-		echo "<td>" . $row['short_desc'] . "</td>";
+	//	echo "<td>" . $row['short_desc'] . "</td>";
 		// echo "<td>" . $row['long_desc'] . "</td>";
-		echo "<td><a href=\"" . $row['course_link'] .  "\" target=\"_blank\">Course Link</a></td>";
+	//	echo "<td><a href=\"" . $row['course_link'] .  "\" target=\"_blank\">Course Link</a></td>";
 		
 		/* removed video for faster loading
 		echo "<td> 
@@ -51,11 +49,11 @@
 
 		echo "<td>" . $row['start_date'] . "</td>";
 		echo "<td>" . $row['course_length'] . "</td>";
-		echo "<td><image src=\"" . $row['course_image'] . "\" alt=\"profimage\" height=\"100\" width=\"100\"></td>";
-		echo "<td>" . $row['category'] . "</td>";
+		echo "<td><a href=\"" . $row['course_link'] . "\" target=\"_blank\"><image src=\"" . $row['course_image'] . "\" alt=\"missing course image\" height=\"100\" width=\"100\"></a></td>";
+	//	echo "<td>" . $row['category'] . "</td>";
 		echo "<td>" . $row['site'] . "</td>";
 		echo "<td>" . $row['profname'] . "</td>";
-		echo "<td><image src=\"" . $row['profimage'] . "\" alt=\"profimage\" height=\"100\" width=\"100\"></td>";
+		echo "<td><image src=\"" . $row['profimage'] . "\" alt=\"missing image\" height=\"100\" width=\"100\"></td>";
 
 		echo "</tr>";
 	}
@@ -64,6 +62,6 @@
 
 
 
-</body>>
+</body>
 
 </html>
